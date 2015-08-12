6f246064c6e98e07c5d0972eac62e4923d3dc7c2
dxjia <piggyguy@sina.cn>
2015-08-12 16:47:22 +0800

    add json output
    Signed-off-by: dxjia <piggyguy@sina.cn>

--- api/v0/handlers/UserHandler.php
+++ api/v0/handlers/UserHandler.php
@@ -1,7 +1,10 @@
 ﻿<?php
 class UserHandler {
     function get($userid) {
-      echo "UserHandler, the request id = ".$userid;
+      echo "UserHandler, the request id = ".$userid."!<br>";
+	  $user = array('id'=>$userid, 'name'=>'David', 'age'=>23);
+	  $output = array('code'=>0, 'message'=>'ok', 'result'=>$user);
+	  exit(json_encode($output));
     }
 }
 ?>
--- api/v0/handlers/UsersHandler.php
+++ api/v0/handlers/UsersHandler.php
@@ -1,7 +1,12 @@
 ﻿<?php
 class UsersHandler {
     function get() {
-      echo "UsersHandler, get all users!";
+		echo "UsersHandler, get all users!<br>";
+		$user1 = array('id'=>10001, 'name'=>'David', 'age'=>23);
+		$user2 = array('id'=>10002, 'name'=>'Jury', 'age'=>30);
+		$users = array($user1, $user2);
+		$output = array('code'=>0, 'message'=>'ok', 'result'=>$users);
+		exit(json_encode($output));
     }
 }
 ?>
